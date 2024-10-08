8716f6ae6e1bd36141c3f2183d0f18e21ffcfc98
kimkihyeon <kimki1125@naver.com>
2024-10-08 21:42:45 +0900

    [feat] 닉네임 설정 화면 완료

---
+++ ranchat/Common/CenterLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  CenterLoadingView.swift
+//  ranchat
+//
+//  Created by 김견 on 10/8/24.
+//
+
+import SwiftUI
+
+struct CenterLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CenterLoadingView()
+}
---
+++ ranchat/Setting/SettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewModel.swift
+//  ranchat
+//
+//  Created by 김견 on 10/8/24.
+//
+
+import Foundation
