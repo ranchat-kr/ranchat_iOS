3768b128e4768c68931a4e8997eb535e86f1346a
kimkihyeon <kimki1125@naver.com>
2024-10-09 21:37:48 +0900

    [feat] ChatInputView분리 / View진입 시, 새 채팅이 생길 시 맨 아래로 스크롤

---
+++ ranchat/Chatting/ChattingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChattingViewModel.swift
+//  ranchat
+//
+//  Created by 김견 on 10/9/24.
+//
+
+import Foundation
---
+++ ranchat/Chatting/CustomView/ChatInputView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatInputView.swift
+//  ranchat
+//
+//  Created by 김견 on 10/9/24.
+//
+
+import SwiftUI
+
+struct ChatInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatInputView()
+}
