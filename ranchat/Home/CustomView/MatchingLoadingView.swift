72a63682d64ac276333a00c6ab46ee6101d191e6
kimkihyeon <kimki1125@naver.com>
2024-10-09 19:02:40 +0900

    [feat] 매칭 다이얼로그 구현 및 연동 / 화면 전환 로직 수정

---
+++ ranchat/Home/CustomView/MatchingLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchingLoadingView.swift
+//  ranchat
+//
+//  Created by 김견 on 10/9/24.
+//
+
+import SwiftUI
+
+struct MatchingLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchingLoadingView()
+}
