d0e3cd3d26506e77f3a0c791d08a58284ab66a39
kimkihyeon <kimki1125@naver.com>
2024-10-17 20:35:15 +0900

    [feat] RoomList 화면 생성 및 이동 구현

---
+++ ranchat/RoomListView.swift
@@ -0,0 +1,18 @@
+//
+//  RoomListView.swift
+//  ranchat
+//
+//  Created by 김견 on 10/17/24.
+//
+
+import SwiftUI
+
+struct RoomListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomListView()
+}
