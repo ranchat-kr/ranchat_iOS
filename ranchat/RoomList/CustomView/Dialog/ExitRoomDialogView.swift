8bb30b7bc6375ec3d542f6f6a93c2389a0606710
kimkihyeon <kimki1125@naver.com>
2024-10-18 20:44:36 +0900

    [feat] 방 리스트 화면에서 왼쪽으로 스와이프 시 해당 방을 나갈건지 묻는 다이얼로그 창을 띄우는 기능 추가.

---
+++ ranchat/RoomList/CustomView/Dialog/ExitRoomDialogView.swift
@@ -0,0 +1,18 @@
+//
+//  ExitRoomDialogView.swift
+//  ranchat
+//
+//  Created by 김견 on 10/18/24.
+//
+
+import SwiftUI
+
+struct ExitRoomDialogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExitRoomDialogView()
+}
