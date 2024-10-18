47c5d42039779ca85cf68de1485588d9f9ec55b6
kimkihyeon <kimki1125@naver.com>
2024-10-18 16:40:56 +0900

    [feat] 룸 리스트 화면에서 맨 아래로 스크롤 시 새로운 데이터 불러오는 기능 추가.

--- ranchat/RoomList/RoomListView.swift
+++ ranchat/RoomList/RoomListView.swift
@@ -11,26 +11,74 @@ struct RoomListView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(IdHelper.self) var idHelper
     @Environment(WebSocketHelper.self) var webSocketHelper
+    
     @State var viewModel = RoomListViewModel()
+    @State private var isScrolledToBottom = false
     
     var body: some View {
-        ScrollView {
-            GeometryReader { geometry in
-                
+        ZStack {
+            ScrollView {
+                VStack {
+                    // 채팅방 리스트
+                    ForEach(Array(viewModel.roomItems.enumerated()), id: \.element.id) { index, roomData in
+                        RoomItemView(roomData: roomData)
+                        
+                        if index < viewModel.roomItems.count - 1 {
+                            Rectangle()
+                                .background(.gray)
+                                .frame(height: 1)
+                                .frame(maxWidth: .infinity)
+                                .padding(.horizontal)
+                        }
+                    }
+                }
+                .padding(.top, -40)
+                .background(
+                    GeometryReader { proxy in
+                        Color.clear
+                            .onChange(of: proxy.frame(in: .global).maxY) { _ in
+                                checkIfScrolledToBottom(proxy: proxy)
+                            }
+                    }
+                )
+            }
+            .onAppear {
+                Task {
+                    await viewModel.getRoomList()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Continue")
+                        .font(.dungGeunMo24)
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    ToolbarButton(action: {
+                        dismiss()
+                    }, imageName: "chevron.backward")
+                }
+            }
+            .navigationBarBackButtonHidden()
+            
+            if viewModel.isLoading {
+                CenterLoadingView()
             }
         }
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                Text("Continue")
-                    .font(.dungGeunMo24)
-            }
-            ToolbarItem(placement: .topBarLeading) {
-                ToolbarButton(action: {
-                    dismiss()
-                }, imageName: "chevron.backward")
+    }
+    
+    private func checkIfScrolledToBottom(proxy: GeometryProxy) {
+        let contentHeight = proxy.size.height
+        let scrollViewHeight = UIScreen.main.bounds.height
+        let scrollOffset = abs(proxy.frame(in: .global).minY)
+
+        print("contentHeight: \(contentHeight), scrollViewHeight: \(scrollViewHeight), scrollOffset: \(scrollOffset)")
+        // Adjust the condition based on your requirement.
+        if  (scrollViewHeight - 50)...scrollViewHeight ~= scrollOffset + contentHeight {
+            // ScrollView가 맨 아래에 도달했을 때 호출될 함수
+            Task {
+                await viewModel.getRoomList()
             }
         }
-        .navigationBarBackButtonHidden()
     }
 }
 
--- ranchat/RoomList/RoomListViewModel.swift
+++ ranchat/RoomList/RoomListViewModel.swift
@@ -11,14 +11,42 @@ class RoomListViewModel {
     var isLoading: Bool = false
     
     var roomPage = 0
-    var roomList: RoomDataList?
+    var roomItems: [RoomData] = [
+//        RoomData(id: 0, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 1, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 2, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 3, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 4, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 5, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 6, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 7, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 8, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 9, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 10, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 11, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 12, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 13, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 14, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 15, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 16, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 17, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 18, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 19, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+//        RoomData(id: 20, title: "qdqd", type: "dqdwq", latestMessage: "dsadad", latestMessageAt: "2024-10-17T21:13:22"),
+    ]
     
     func getRoomList() async {
         isLoading = true
         
         do {
-            roomList = try await ApiHelper.shared.getRooms(page: roomPage, size: 10)
+            let roomList = try await ApiHelper.shared.getRooms(page: roomPage, size: 10)
+            if roomList.data.totalCount == roomItems.count {
+                return
+            }
             roomPage += 1
+            roomItems.append(contentsOf: roomList.data.items)
+            print("roomcount: \(roomItems.count), roomList: \(roomList)")
+            try await Task.sleep(for: .seconds(2))
         } catch {
             print("DEBUG: RoomListViewModel.getRoomList() error: \(error.localizedDescription)")
         }
