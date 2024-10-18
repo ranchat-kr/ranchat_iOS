f9e7d6193834d1055fdcc5d3bbb75fbcd4957035
kimkihyeon <kimki1125@naver.com>
2024-10-18 19:49:29 +0900

    [chore] 방 리스트 화면에서 제일 마지막 방이 보이면 새로운 방 리스트를 호출하도록 수정

--- ranchat/RoomList/RoomListView.swift
+++ ranchat/RoomList/RoomListView.swift
@@ -19,23 +19,30 @@ struct RoomListView: View {
         ZStack {
             List(Array(viewModel.roomItems.enumerated()), id: \.element.id) { index, roomData in
                 // 채팅방 리스트
-//                ForEach(Array(viewModel.roomItems.enumerated()), id: \.element.id) { index, roomData in
-                    RoomItemView(roomData: roomData)
+                //                ForEach(Array(viewModel.roomItems.enumerated()), id: \.element.id) { index, roomData in
+                RoomItemView(roomData: roomData)
                     .listRowInsets(EdgeInsets())
-                        .swipeActions(edge: .trailing) {
-                            Button(role: .destructive) {
-                                withAnimation {
-                                    viewModel.exitRoom(at: index)
-                                }
-                            } label: {
-                                Text("나가기")
-                                    .font(.dungGeunMo16)
-                                    .foregroundStyle(.white)
-                                    .padding()
-                                    .background(Color.red)
+                    .swipeActions(edge: .trailing) {
+                        Button(role: .destructive) {
+                            withAnimation {
+                                viewModel.exitRoom(at: index)
+                            }
+                        } label: {
+                            Text("나가기")
+                                .font(.dungGeunMo16)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(Color.red)
+                        }
+                    }
+                    .onAppear {
+                        if viewModel.roomItems.last?.id == roomData.id {
+                            Task {
+                                await viewModel.getRoomList()
                             }
                         }
-//                }
+                    }
+                //                }
             }
             .listStyle(.plain)
             .onAppear {
